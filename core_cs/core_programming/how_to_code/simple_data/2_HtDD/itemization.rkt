c5507c544db2d77460eaa1da0c7582b67a160aba
pei-pay <gleektennis@icloud.com>
2023-02-19 15:51:21 +0900

    add: HtDF itemization

--- core_cs/core_programming/how_to_code/simple_data/2_HtDD/itemization.rkt
+++ core_cs/core_programming/how_to_code/simple_data/2_HtDD/itemization.rkt
@@ -1,6 +1,10 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname itemization) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+;; Data definitons
+
 ;; Countdown is one of:
 ;;  - false
 ;;  - Natural[1, 10]
@@ -28,3 +32,23 @@
 ;;   - atomic distinct: false
 ;;   - atomic non-distinct: Natural[1, 10]
 ;;   - atomic distinct: "complete"
+
+
+;; Functions
+
+;; Countdonw -> Image
+;; Produce nice image of current of countdown
+
+(check-expect (countdown-to-image false) (square 0 "solid" "white"))
+(check-expect (countdown-to-image 5) (text (number->string 5) 24 "black"))
+(check-expect (countdown-to-image "complete") (text "Happy New Year!" 24 "red"))
+
+; (define (countdown-to-image c) (square 0 "solid" "white")) ;stub
+
+
+; <use template form CountDown>
+
+(define (countdown-to-image c)
+  (cond [(false? c) (square 0 "solid" "white")]
+        [(and (number? c) (<= 1 c) (<= c 10)) (text (number->string c) 24 "black")]
+        [else (text "Happy New Year!" 24 "red")]))
