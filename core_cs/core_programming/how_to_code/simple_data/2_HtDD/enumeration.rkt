1306d4e40eddda3efbbd27e4bf0bba14cc6414f6
pei-pay <gleektennis@icloud.com>
2023-02-19 15:38:25 +0900

    add: HtDF enumeration

--- core_cs/core_programming/how_to_code/simple_data/2_HtDD/enumeration.rkt
+++ core_cs/core_programming/how_to_code/simple_data/2_HtDD/enumeration.rkt
@@ -1,6 +1,8 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname enumeration) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Data definitons
+
 ;; LetterGrade is one of:
 ;; - "A"
 ;; - "B"
@@ -20,3 +22,25 @@
 ;; - atomic distinct value: "A"
 ;; - atomic distinct value: "B"
 ;; - atomic distinct value: "C"
+
+
+
+;; Functions
+
+;; LeterGrade -> LetterGrade
+;; produce next highest letter grade (no change for A)
+
+(check-expect (bump-up "A") "A")
+(check-expect (bump-up "B") "A")
+(check-expect (bump-up "C") "B")
+
+
+; (define (bump-up lg) "A") ;stub
+
+
+; <use template form LetterGrade>
+
+(define (bump-up lg)
+  (cond [(string=? lg "A") "A"]
+        [(string=? lg "B") "A"]
+        [(string=? lg "C") "B"]))
